44d8ea54ae59b8dc5fc9267933ee9999774e9cf8
Erik <hfvanroon@gmail.com>
2017-10-09 16:57:10 +0200

    add zot2.groovy

--- src/org/foo/Zot2.groovy
+++ src/org/foo/Zot2.groovy
@@ -3,3 +3,14 @@ package org.foo;
 def echoStuff(stuff) {
   echo "${stuff}"
 }
+
+def mail(recipients, subject, replyTo, from, body){
+  mail \
+    bcc: '', \
+    body: body, \
+    cc: '', \
+    from: from \
+    replyTo: replyTo, \
+    subject: subject, \
+    to: recipients
+}
